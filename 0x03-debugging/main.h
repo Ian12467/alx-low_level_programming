8060a331a45b15c069886739978d23d7ed476562
Ian12467 <ianochieng277@gmail.com>
2022-06-09 03:33:41 -0700

    debug

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,13 +1,29 @@
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+/**
+ * * main - Entrypoint
+ * * Return: Always 0 (success)
+ *  */
+int main(void)
+{
+	int n;
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+
+	return (0);
+}
+#endif /* MAIN_H */
