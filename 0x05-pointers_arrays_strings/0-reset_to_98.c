d2cee2dfb749d8a399a0b4e5b32c65574caa3a7d
Ian12467 <ianochieng277@gmail.com>
2022-06-13 13:27:13 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /*
- * reset_to_98.c - updates the value of the variable the pointer points to to 98
+ * 0-reset_to_98.c - updates the value of the variable
+ * the pointer points to to 98
  * @n: pointer to the variable to update
  */
 void reset_to_98(int *n)
