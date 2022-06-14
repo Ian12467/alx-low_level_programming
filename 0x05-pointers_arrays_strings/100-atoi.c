d41eb470a9aa9f9148dc8c3fd7e49f98829b27d8
Ian12467 <ianochieng277@gmail.com>
2022-06-13 17:28:35 -0700

    my PROGRAM

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/**_atoi - converts a string to an integer
+/**
+ * _atoi - converts a string to an integer
  * @s: string to be converted
  * Return: the int converted from the string
  */
