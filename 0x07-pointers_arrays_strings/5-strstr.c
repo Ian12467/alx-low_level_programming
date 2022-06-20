c72823b80ae70ff3970be70f1e9c7ca718e0da32
Ian12467 <ianochieng277@gmail.com>
2022-06-20 04:19:54 -0700

    second bit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,11 +13,11 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (j = 0; needle[j] != '\0'; j++)
+	for (i = 0; needle[j] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if haystack[i + j] != needle[j])
+			if (haystack[i + j] != needle[j])
 				break;
 		}
 		if (!needle[j])
