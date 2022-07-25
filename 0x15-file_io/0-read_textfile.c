7be02545fb5fc9adb610c572c025667b6907f535
Ian12467 <64345032+Ian12467@users.noreply.github.com>
2022-07-26 02:58:00 +0300

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,41 @@
 #include "main.h"
+#include <string.h>
+
 
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
+ * read_textfile - reads the content of the file
+ * @filename: name of the file
+ * @letters: number of letters to be printed
  *
- * Return: the number of letters printed, or 0 if it failed
+ * Return: always 0 success
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int s, t;
-	char *buf;
+	int x;
+	ssize_t frd, fwr;
+	char *out;
 
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	x = open(filename, O_RDONLY);
+
+	if (x == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+	out = malloc(sizeof(char) * (letters));
+
+	if (!out)
 		return (0);
 
-	s = read(fd, buf, letters);
-	if (s < 0)
-	{
-		free(buf);
+	frd = read(x, out, letters);
+	if (frd == -1)
 		return (0);
-	}
-	buf[s] = '\0';
+	fwr = write(STDOUT_FILENO, out, frd);
 
-	close(fd);
+	free(out);
+	close(x);
 
-	t = write(STDOUT_FILENO, buf, s);
-	if (t < 0)
-	{
-		free(buf);
-		return (0);
-	}
-
-	free(buf);
-	return (t);
+	return (fwr);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,37 +1,39 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
- *
- * Return: 1 if it success. -1 if it fails.
+ * create_file - function that creates a file
+ * @filename: pointer to name of file to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure, or if filename is NULL
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int o, w, num = 0;
 
-	if (!filename)
+	if (filename == NULL)
+
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (text_content != NULL)
 
-	if (fd == -1)
+	{
+
+		for (num = 0; text_content[num];)
+
+			num++;
+
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	w = write(o, text_content, num);
+
+	if (o == -1 || w == -1)
+
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
-	close(fd);
+	close(o);
 	return (1);
+
 }
